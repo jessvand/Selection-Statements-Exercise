2353190990c699d45e3986f1f4cf461282c4b1ac
jessvand <jmvandbeek@gmail.com>
2024-05-02 13:16:32 -0400

    added favorite Number code

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            int favNum = r.Next(2, 12);
+            int userInput = 0;
+
+            while (userInput != favNum)
+            {
+
+                Console.WriteLine("Guess my favorite number (2-12):");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNum)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {userInput}");
+                }
+
+                else if (userInput > favNum)
+                {
+                    Console.WriteLine($"Too high!\nYour guess: {userInput}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"YEEAAHHH! You got it!\nYour guess: {userInput}");
+                }
+            }
+
         }
     }
+
+
+
+    
 }
